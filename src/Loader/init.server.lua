a69a0ffd2f661802c78137c36522695a02e4696a
cameronnh <44270403+cameronnh@users.noreply.github.com>
2025-05-19 22:15:28 -0400

    we starting

---
+++ src/Loader/init.server.lua
@@ -0,0 +1,7 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerStorage = game:GetService("ServerStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+
+
+print("Starting To Load Modules...")
