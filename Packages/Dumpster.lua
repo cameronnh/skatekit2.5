cf5d9ee6cc5cbbf3efdccd2c9a8cfdaa8bdde761
cameronnh <44270403+cameronnh@users.noreply.github.com>
2025-05-19 22:20:50 -0400

    loader

--- Packages/Dumpster.lua
+++
@@ -1,1 +0,0 @@
-return require(script.Parent._Index["kinqandi_dumpster@0.3.8"]["dumpster"])
--- src/Loader/init.server.lua
+++ src/Loader/init.server.lua
@@ -1,7 +1,15 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local ServerStorage = game:GetService("ServerStorage")
 local ServerScriptService = game:GetService("ServerScriptService")
+local StarterPlayerScripts = game:GetService("StarterPlayer"):WaitForChild("StarterPlayerScripts")
 
-
+local function parentModules(folderOfModules: Folder, parent: any): ()
+    for _: number, child: any in folderOfModules:GetChildren() do
+        child.Parent = parent
+    end
+end
 
 print("Starting To Load Modules...")
+
+parentModules(script:WaitForChild("ReplicatedStorage"), ReplicatedStorage)
+parentModules(script:WaitForChild("ServerScriptService"), ServerScriptService)
+parentModules(script:WaitForChild("StarterPlayerScripts"), StarterPlayerScripts)
